1ce231ea31c4f1e2ec45574cdbd27a70b9e2996e
Stingray <whongyu76@gmail.com>
2025-04-25 14:50:06 +0800

    Create Worst-case_main.cpp

---
+++ Quick Sort/Worst-case_main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <chrono>
+#include <windows.h>
+#include <psapi.h>
+
+using namespace std;
+using namespace std::chrono;
+
+void quickSort(vector<int> arr, int left, int right) {
+    if (left < right) {
+        int pivot = arr[right];
+        int i = left - 1;
+        for (int j = left; j < right; ++j) {
+            if (arr[j] <= pivot) {
+                ++i;
+                swap(arr[i], arr[j]);
+            }
+        }
+        swap(arr[i + 1], arr[right]);
+        int pivotIndex = i + 1;
+
+        quickSort(arr, left, pivotIndex - 1);
+        quickSort(arr, pivotIndex + 1, right);
+    }
+}
+
+vector<int> WorstCase(int n) {
+    vector<int> arr(n);
+    for (int i = 0; i < n; ++i) {
+        arr[i] = n - i;
+    }
+    return arr;
+}
+
+void printMemoryUsage() {
+    PROCESS_MEMORY_COUNTERS pmc;
+    if (GetProcessMemoryInfo(GetCurrentProcess(), &pmc, sizeof(pmc))) {
+        cout << "Working Set Size: " << pmc.WorkingSetSize / 1024 / 1024 << " MB\n"
+             << "Peak Working Set Size: " << pmc.PeakWorkingSetSize / 1024 / 1024 << " MB\n"
+             << "Pagefile Usage: " << pmc.PagefileUsage / 1024 / 1024 << " MB\n" << endl;
+    }
+}
+
+int main() {
+    srand(time(0));
+
+    vector<int> sizes = { 500, 1000 , 2000, 3000, 4000, 5000};
+
+    for (int n : sizes) {
+        vector<int> arr = WorstCase(n);
+
+        auto start = high_resolution_clock::now();
+        quickSort(arr, 0, arr.size() - 1);
+        auto end = high_resolution_clock::now();
+
+        auto duration = duration_cast<microseconds>(end - start);
+        cout << "n = " << n << endl << "Worst-case time : " << duration.count() << " microseconds\n";
+        printMemoryUsage();
+    }
+    return 0;
+}
