22879f8b42dcf43b7f840d0c027723431d62fcb7
ChiHangXie <wanxie58@gmail.com>
2025-04-28 23:53:06 +0800

    Update Merge Sort_interactive_version_worst_case_test_program.cpp

--- Merge Sort/Merge Sort_interactive_version_worst_case_test_program.cpp
+++ Merge Sort/Merge Sort_interactive_version_worst_case_test_program.cpp
@@ -43,13 +43,10 @@ void merge_sort_v(int* array, int size) {
     int* dest = temp;
 
     for (int width = 1; width < size; width *= 2) {
-        printf("Merging subarrays of size %d (run 1 → temp):\n", width);
         for (int i = 0; i < size; i += 2 * width) {
             int left = i;
             int mid = (i + width < size) ? i + width : size;
             int right = (i + 2 * width < size) ? i + 2 * width : size;
-
-            printf("  Merging [%d:%d] and [%d:%d] → ", left, mid - 1, mid, right - 1);
             merge(src, dest, left, mid, right);
         }
 
@@ -69,26 +66,42 @@ void merge_sort_v(int* array, int size) {
     free(temp);
 }
 
+void fill_worst(int* arr, int size) {
+    for (int i = 0; i < size; ++i) {
+        arr[i] = size - i;
+    }
+}
 
 int main() {
     int pre_sort_array[MAX_INPUT_SIZE+1] = {0}, i = 0;//+1 is for the end of input flag aka -1
+    int test_element_count, test_repeat = 10000;
+    double avg_duration=0;
 
-    while (1) {
-        scanf("%d", &pre_sort_array[i]);
-        if (pre_sort_array[i] == -1) break;
-        i++;
-    }
-
-    clock_t start = clock();
-    merge_sort_v(pre_sort_array,i);
-    clock_t end = clock();
-
-    double duration = (double)(end - start) / CLOCKS_PER_SEC;
-    printf("MergeSort interactive version duration: %.9lf seconds\n", duration);
+    while (1) 
+    {
+        scanf("%d", &test_element_count);
+        if (test_element_count == -1) break;
     
-    printf("Memory Usage Info (from /proc/self/status):\n");
-    MemoryUsage();
-
+        i = test_repeat;
+        
+        fill_worst(pre_sort_array,test_element_count);
+    
+        while(i--){
+        
+            clock_t start = clock();
+            merge_sort_v(pre_sort_array,test_element_count);
+            clock_t end = clock();
+        
+            avg_duration += (double)(end - start) / CLOCKS_PER_SEC;
+        }
+        
+        printf("MergeSort interactive version total duration: %.9lf seconds\n", avg_duration);
+        printf("MergeSort interactive version average duration: %.9lf seconds\n", avg_duration/test_repeat);
+    
+        printf("Memory Usage Info (from /proc/self/status):\n");
+        MemoryUsage();
+    
+    }
 
     return 0;
 }
