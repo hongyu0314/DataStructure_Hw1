2e91959738682b4ffd1a75bd84833c6f0f87bef2
ChiHangXie <wanxie58@gmail.com>
2025-04-29 00:21:48 +0800

    Update Merge Sort_interactive_version_averge_time_test_program.cpp

--- Merge Sort/Merge Sort_interactive_version_averge_time_test_program.cpp
+++ Merge Sort/Merge Sort_interactive_version_averge_time_test_program.cpp
@@ -66,28 +66,29 @@ void merge_sort_v(int* array, int size) {
     free(temp);
 }
 
-void fill_random(int* arr, int size, int range) {
+void fill_random(int* arr, int size) {
+    int range = size;
     srand(time(0));
     for (int i = 0; i < size; ++i) {
         arr[i] = rand() % range;  // [0, range-1]
     }
 }
 
-
 int main() {
     int pre_sort_array[MAX_INPUT_SIZE+1] = {0}, i = 0;//+1 is for the end of input flag aka -1
-    int test_element_count = 1000, test_repeat;
+    int test_element_count, test_repeat = 10000;
     double avg_duration=0;
 
     while (1) 
     {
-        scanf("%d", &test_repeat);
-        if (test_repeat == -1) break;
+        scanf("%d", &test_element_count);
+        if (test_element_count == -1) break;
     
         i = test_repeat;
     
         while(i--){
-            fill_random(pre_sort_array,test_element_count,2000);
+        
+        fill_random(pre_sort_array,test_element_count);
         
             clock_t start = clock();
             merge_sort_v(pre_sort_array,test_element_count);
@@ -95,6 +96,7 @@ int main() {
         
             avg_duration += (double)(end - start) / CLOCKS_PER_SEC;
         }
+        
         printf("MergeSort interactive version total duration: %.9lf seconds\n", avg_duration);
         printf("MergeSort interactive version average duration: %.9lf seconds\n", avg_duration/test_repeat);
     
