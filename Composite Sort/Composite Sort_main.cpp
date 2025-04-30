36fb152be141cce874a2ff8dd0bd87a5b183df85
Stingray <whongyu76@gmail.com>
2025-04-30 18:00:34 +0800

    Update Composite Sort_main.cpp

--- Composite Sort/Composite Sort_main.cpp
+++ Composite Sort/Composite Sort_main.cpp
@@ -9,7 +9,6 @@
 using namespace std;
 using namespace std::chrono;
 
-// Insertion
 template <typename T>
 void insertionSort(T* arr, int size) {
     for (int i = 1; i < size; i++) {
@@ -24,27 +23,6 @@ void insertionSort(T* arr, int size) {
     }
 }
 
-// Quick
-template <typename T>
-void quickSort(T* arr, int left, int right) {
-    if (left < right) {
-        T pivot = arr[right];
-        int i = left - 1;
-        for (int j = left; j < right; ++j) {
-            if (arr[j] <= pivot) {
-                ++i;
-                swap(arr[i], arr[j]);
-            }
-        }
-        swap(arr[i + 1], arr[right]);
-        int pivotIndex = i + 1;
-
-        quickSort(arr, left, pivotIndex - 1);
-        quickSort(arr, pivotIndex + 1, right);
-    }
-}
-
-// Merge
 void merge(int* src, int* dest, int left, int mid, int right) {
     int i = left, j = mid, k = left;
     while (i < mid && j < right) {
@@ -85,7 +63,6 @@ void merge_sort_v(int* array, int size) {
     free(temp);
 }
 
-// Heap
 void localHeapSort(int* arr, int n, int i) {
     int largest = i;
     int left = 2 * i + 1;
@@ -113,22 +90,54 @@ void heapsort(int* arr, int n) {
     }
 }
 
-void printMemoryUsage() {
+void quickSort(vector<int>& arr, int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        int pivotIndex = left;
+        if (arr[mid] < arr[left])
+            pivotIndex = mid;
+        if (arr[right] < arr[pivotIndex])
+            pivotIndex = right;
+        swap(arr[pivotIndex], arr[right]);
+
+        int pivot = arr[right];
+        int i = left - 1;
+        for (int j = left; j < right; ++j) {
+            if (arr[j] <= pivot) {
+                ++i;
+                swap(arr[i], arr[j]);
+            }
+        }
+        swap(arr[i + 1], arr[right]);
+        int pivotIndexNew = i + 1;
+
+        if (pivotIndexNew - 1 < right - pivotIndexNew) {
+            quickSort(arr, left, pivotIndexNew - 1);
+            quickSort(arr, pivotIndexNew + 1, right);
+        }
+        else {
+            quickSort(arr, pivotIndexNew + 1, right);
+            quickSort(arr, left, pivotIndexNew - 1);
+        }
+    }
+}
+
+void MemoryUsage() {
     PROCESS_MEMORY_COUNTERS pmc;
     if (GetProcessMemoryInfo(GetCurrentProcess(), &pmc, sizeof(pmc))) {
-        cout << "Working Set Size: " << pmc.WorkingSetSize / 1024 / 1024 << " MB\n"
-            << "Peak Working Set Size: " << pmc.PeakWorkingSetSize / 1024 / 1024 << " MB\n"
-            << "Pagefile Usage: " << pmc.PagefileUsage / 1024 / 1024 << " MB\n" << endl;
+        cout << "Working Set Size: " << pmc.WorkingSetSize / 1024 << " KB\n"
+            << "Peak Working Set Size: " << pmc.PeakWorkingSetSize / 1024  << " KB\n"
+            << "Pagefile Usage: " << pmc.PagefileUsage / 1024<< " KB\n" << endl;
     }
 }
 
 template <typename T>
-void compositeSort(T* arr, int size) {
+void compositeSort(T& arr, int size) {
     if (size <= 10) {
-        insertionSort(arr, size);
+        insertionSort(arr.data(), size);
     }
     else if (size < 1000 && size > 10) {
-        heapsort(arr, size);
+        heapsort(arr.data(), size);
     }
     else {
         quickSort(arr, 0, size - 1);
@@ -138,7 +147,7 @@ void compositeSort(T* arr, int size) {
 int main() {
     srand(time(0));
 
-    vector<int> sizes = { 10, 300, 500, 1000, 2000, 5000 };
+    vector<int> sizes = { 50, 500, 1000, 3000, 5000, 10000 };
 
     for (int n : sizes) {
         vector<int> arr(n);
@@ -147,12 +156,12 @@ int main() {
         }
 
         auto start = high_resolution_clock::now();
-        compositeSort(arr.data(), n);
+        compositeSort(arr, n);
         auto end = high_resolution_clock::now();
 
         auto duration = duration_cast<microseconds>(end - start);
         cout << "n = " << n << endl << "Composite Sort time: " << duration.count() << " microseconds" << endl;
-        printMemoryUsage();
+        MemoryUsage();
     }
 
     return 0;
